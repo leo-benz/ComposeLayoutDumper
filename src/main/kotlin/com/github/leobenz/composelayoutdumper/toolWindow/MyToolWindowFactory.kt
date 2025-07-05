db27fc17ab0149ab65c4c6a353a7ba807caf8e01
GitHub Action <action@github.com>
2025-07-05 18:30:28 +0000

    Template cleanup

--- src/main/kotlin/com/github/leobenz/composelayoutdumper/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/leobenz/composelayoutdumper/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.leobenz.composelayoutdumper.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.leobenz.composelayoutdumper.MyBundle
+import com.github.leobenz.composelayoutdumper.services.MyProjectService
 import javax.swing.JButton
 
 
